d318a4a670254801096113d4521932319a4e35d0
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-29 16:54:58 -0600

    prepare for lesson 5

--- SheCodesPythonWorkshop_Week-3.py
+++ SheCodesPythonWorkshop_Week-3.py
@@ -25,19 +25,21 @@
 #display_current_weather("Wichita", "5")
 
 # lesson 4
-def display_current_weather(current_city, current_temperature, current_humidity=""):
-    """Request current location and weather details"""
-    if current_city and current_temperature and current_humidity : 
-        print(f"The temperature in {current_city.capitalize()} is {current_temperature}ºC. with a humidity of {current_humidity}%")
-    elif current_city and current_temperature : 
-        print(f"The temperature in {current_city} is {current_temperature}ºC.")
-    else:
-        print("Please provide a city and temperature")
-    
-current_city = input("What is the city you are currently in? ")
-current_temperature = input("What is the current temperature in celcius? ")
-current_humidity = input("What is the current percent humidity? ")    
+#def display_current_weather(current_city, current_temperature, current_humidity=""):
+#    """Request current location and weather details"""
+#    if current_city and current_temperature and current_humidity : 
+#        print(f"The temperature in {current_city.capitalize()} is {current_temperature}ºC. with a humidity of {current_humidity}%")
+#    elif current_city and current_temperature : 
+#        print(f"The temperature in {current_city} is {current_temperature}ºC.")
+#    else:
+#        print("Please provide a city and temperature")
+#    
+#current_city = input("What is the city you are currently in? ")
+#current_temperature = input("What is the current temperature in celcius? ")
+#current_humidity = input("What is the current percent humidity? ")    
 
-display_current_weather(current_city, current_temperature, current_humidity)
-display_current_weather("Fall Creek", "1", "65")
-display_current_weather("Wichita", "5", "")
+#display_current_weather(current_city, current_temperature, current_humidity)
+#display_current_weather("Fall Creek", "1", "65")
+#display_current_weather("Wichita", "5", "")
+
+# lesson 5
