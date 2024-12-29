7d48345a86b3691dd7e86859e660afa136bfab1c
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-29 17:13:40 -0600

    Update to workshops for lesson 5 on python function returns

--- SheCodesPythonWorkshop_Week-3.py
+++ SheCodesPythonWorkshop_Week-3.py
@@ -43,3 +43,15 @@
 #display_current_weather("Wichita", "5", "")
 
 # lesson 5
+def calculate_farenheit (temperature_celcius) :
+  """Return the Farenheit value of a celcius temperature"""
+  temperature_farenheit = (float(temperature_celcius) * 9 / 5) +32
+  
+  return temperature_farenheit
+
+temperature_celcius = 15
+
+temperature_farenheit = calculate_farenheit(temperature_celcius)
+city = "London"
+
+print(f"It is currently {temperature_celcius}ºC ({temperature_farenheit}ºF) in {city}")
