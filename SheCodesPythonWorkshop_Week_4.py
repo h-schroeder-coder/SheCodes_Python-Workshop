9e7ca0f5f895c1858844d8e1301b6f4225aa8795
h-schroeder-coder <hankorschroeder@gmail.com>
2024-12-29 18:03:12 -0600

    Create python for week 4
    added content for lesson 2 with lists

---
+++ SheCodesPythonWorkshop_Week_4.py
@@ -0,0 +1,19 @@
+# lesson 2
+# Create a list of 3 countries you’d like to visit
+countries = ["France", "Italy", "Ireland"]
+# Print out the list
+print(countries)
+# Remove the last country
+countries.pop()
+# Print out the list
+print(countries)
+# Add another element at the beginning of the list
+countries.append("Greece")
+# Print out the list
+print(countries)
+# Print out the length of the list
+print(len(countries))
+# Sort the list 
+countries.sort()
+# Print out the list
+print(countries)
